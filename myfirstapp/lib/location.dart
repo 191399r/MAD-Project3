67f82f64383d1c417e48904ce76d5fbaa430a40d
191399r <75418474+191399r@users.noreply.github.com>
2021-02-18 21:27:32 +0800

    firebase construction and maps improvement

--- myfirstapp/lib/location.dart
+++ myfirstapp/lib/location.dart
@@ -13,10 +13,33 @@ class _LocationState extends State<Location> {
   Completer<GoogleMapController> _controller = Completer();
 
   static const LatLng _center = const LatLng(1.29684825487647, 103.85253591654006);
+  LatLng _lastMapPosition = _center;
 
   void _onMapCreated(GoogleMapController controller) {
     _controller.complete(controller);
+    setState(() {
+      _markers.add(
+          Marker(
+            markerId: MarkerId(''),
+            position: _center,
+          ));
+    });
   }
+
+  void _onMapTypeButtonPressed() {
+    setState(() {
+      _currentMapType = _currentMapType == MapType.normal
+          ? MapType.satellite
+          : MapType.normal;
+    });
+  }
+
+  void _onCameraMove(CameraPosition position){
+    _lastMapPosition = position.target;
+  }
+
+  MapType _currentMapType = MapType.normal;
+  final Set<Marker> _markers = {};
 
   @override
   Widget build(BuildContext context) {
@@ -36,12 +59,30 @@ class _LocationState extends State<Location> {
           actions: <Widget>[
           ],
         ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 30.0,
-          ),
+        body: Stack(
+          children: <Widget>[
+            GoogleMap(
+              markers: _markers,
+              mapType: _currentMapType,
+              onMapCreated: _onMapCreated,
+              initialCameraPosition: CameraPosition(
+                target: _center,
+                zoom: 25.0,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Align(
+                alignment: Alignment.topRight,
+                child: FloatingActionButton(
+                  onPressed: _onMapTypeButtonPressed,
+                  materialTapTargetSize: MaterialTapTargetSize.padded,
+                  backgroundColor: Colors.green,
+                  child: const Icon(Icons.map, size: 36.0),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
